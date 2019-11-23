1e580ec45240f3da183885b42ac28c2265f8255f
Wooyon <48887513+Wooyon@users.noreply.github.com>
2019-11-23 15:24:20 +0900

    #1.3 Asking for Permissions

--- App.js
+++ App.js
@@ -1,18 +1,30 @@
 import React from "react";
+import { Alert } from "react-native";
 import Loading from "./Loading";
 import * as Location from "expo-location";
-import * as Permissions from "expo-permissions";
 
 export default class extends React.Component {
+  state = {
+    isLoading: true
+  };
+
   getLoaction = async () => {
-    const location = await Location.getCurrentPositionAsync();
-    console.log(location);
+    try {
+      await Location.requestPermissionsAsync();
+      const {
+        coords: { latitude, longitude }
+      } = await Location.getCurrentPositionAsync();
+      this.setState({ isLoadins: false });
+    } catch (error) {
+      Alert.alert("Can't find you.", "So sad");
+    }
   };
 
   componentDidMount() {
     this.getLoaction();
   }
   render() {
-    return <Loading />;
+    const { isLoading } = this.state;
+    return isLoading ? <Loading /> : null;
   }
 }
