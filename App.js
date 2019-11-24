bee30bf5fbd076f96470976f18020afcb82f7cb3
Wooyon <48887513+Wooyon@users.noreply.github.com>
2019-11-25 01:29:52 +0900

    #2.1 Getting the condition names

--- App.js
+++ App.js
@@ -6,16 +6,27 @@ import axios from "axios";
 import Weather from "./Weather.js";
 
 const API_KEY = "4078f6f18b657762382dc0fc9bc05a02";
+const i = 0;
 
 export default class extends React.Component {
   state = {
     isLoading: true
   };
   getWeather = async (latitude, longitude) => {
-    const { data } = await axios.get(
+    const {
+      data: {
+        main: { temp },
+        weather
+      }
+    } = await axios.get(
       `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}&units=metric`
     );
-    this.setState({ isLoading: false, temp: data.main.temp });
+
+    this.setState({
+      isLoading: false,
+      temp,
+      condition: weather[0].main
+    });
   };
 
   getLoaction = async () => {
@@ -34,8 +45,12 @@ export default class extends React.Component {
     this.getLoaction();
   }
   render() {
-    const { isLoading, temp } = this.state;
+    const { isLoading, temp, condition } = this.state;
 
-    return isLoading ? <Loading /> : <Weather temp={temp} />;
+    return isLoading ? (
+      <Loading />
+    ) : (
+      <Weather temp={temp} condition={condition} />
+    );
   }
 }
--- Weather.js
+++ Weather.js
@@ -1,17 +1,36 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Icon } from "react-native";
 import PropTypes from "prop-types";
 
-export default function Weather({ temp }) {
+export default function Weather({ temp, condition }) {
   return (
     <View style={styles.container}>
       <Text>{temp}</Text>
+      <Text>{condition}</Text>
     </View>
   );
 }
 
 Weather.proptypes = {
-  temp: PropTypes.number.isRequired
+  temp: PropTypes.number.isRequired,
+  condition: PropTypes.oneOf([
+    "Thunderstorm",
+    "Drizzle",
+    "Rain",
+    "Snow",
+    "Atmosphere",
+    "Clear",
+    "Clouds",
+    "Haze",
+    "Mist",
+    "Smoke",
+    "Dust",
+    "Fog",
+    "Sand",
+    "Ash",
+    "Squall",
+    "Tornado"
+  ]).isRequired
 };
 
 const styles = StyleSheet.create({
